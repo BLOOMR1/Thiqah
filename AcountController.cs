c82cd1d1a7cbf8e1512661d4abc05aaf8d88491f
Arwa <89831900+BLOOMR1@users.noreply.github.com>
2023-08-13 10:38:48 +0300

    Add files via upload

---
+++ AcountController.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.UI.WebControls;
+using Thiaqah.Models;
+
+namespace Thiaqah.Controllers
+{
+    public class AcountController : Controller
+    {
+        // GET: Acount
+        public ActionResult Index()
+        {
+            //return View();
+            using (OurDBContext db = new OurDBContext())
+            {
+                return View(db.userAccount.ToList());
+            }
+        }
+
+        public ActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Register(UserAccount account)
+        {
+            if (ModelState.IsValid)
+            {
+                using (OurDBContext db = new OurDBContext())
+                {
+                    db.userAccount.Add(account);
+                    db.SaveChanges();
+                }
+                ModelState.Clear();
+                ViewBag.Message = account.FirstName + "" + account.LastName + " Has successfully enrolled.";
+            }
+            return View();
+        }
+        //login
+        public ActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Login(UserAccount user)
+        {
+            using (OurDBContext db = new OurDBContext())
+            {
+                var usr = db.userAccount.Single(u => u.Email == user.Email && u.Password == user.Password);
+                if (usr != null)
+                {
+                    Session["UserID"] = usr.UserID.ToString();
+                    Session["Email"] = usr.Email.ToString();
+                    return RedirectToAction("Loggedin");
+                }
+                else
+                {
+                    ModelState.AddModelError("", "Email Or Password is incorrect");
+                }
+            }
+            return View();
+        }
+        public ActionResult Loggedin()
+        {
+            if (Session["UserID"] != null)
+            {
+                return View();
+            }
+            else
+            {
+                return RedirectToAction("Login");
+            }
+        }
+    }
+
+
+}
---
+++ UserAccount.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Thiaqah.Models
+{
+    public class UserAccount
+    {
+        [Key]
+        public int UserID { get; set; }
+
+        [Required(ErrorMessage = "First Name Is Required!.")]
+        public string FirstName { get; set; }
+
+        [Required(ErrorMessage = "Last Name Is Required!.")]
+        public string LastName { get; set; }
+
+
+        [Required(ErrorMessage = "Email Is Required!.")]
+        [RegularExpression(@"^([\w-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3]\.[0-9]{1,3}\.)|(([\w-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$", ErrorMessage = "Please Enter a Valid Email.")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Birth Date Is Required!.")]
+        [DataType(DataType.Date)]
+        [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{yyyy.MM.dd}")]
+        public DateTime BirthDate { get; set; }
+
+        [Required(ErrorMessage = "Password Is Required!.")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+        [Compare("Password", ErrorMessage = "Please confirm your password.")]
+        [DataType(DataType.Password)]
+        public string ConformPassword { get; set; }
+    }
+}
